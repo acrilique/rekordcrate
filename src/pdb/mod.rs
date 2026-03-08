de4963024f2430d9f9f9405307a0b7acebfa01c4
Lluc Simó <lluc.simo@protonmail.com>
2026-03-08 18:53:07 +0100

    feat(pdb): Implement RowVariant for Menu

--- src/pdb/mod.rs
+++ src/pdb/mod.rs
@@ -2033,6 +2033,23 @@ impl PageHeapObject for Menu {
     }
 }
 
+impl RowVariant for Menu {
+    const PAGE_TYPE: PageType = PageType::Plain(PlainPageType::Menu);
+
+    fn from_row(row: &Row) -> Option<&Self> {
+        match row {
+            Row::Plain(PlainRow::Menu(row)) => Some(row),
+            _ => None,
+        }
+    }
+    fn from_row_mut(row: &mut Row) -> Option<&mut Self> {
+        match row {
+            Row::Plain(PlainRow::Menu(row)) => Some(row),
+            _ => None,
+        }
+    }
+}
+
 /// A table row contains the actual data.
 #[binrw]
 #[derive(Debug, PartialEq, Eq, Clone)]
